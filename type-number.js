e3dcd5884c6f8d6b8144d3bef22a85aa46550c32
Thiago Andrade <thicma@gmail.com>
2022-05-03 13:54:40 -0300

    Modulo 1

---
+++ type-number.js
@@ -0,0 +1,26 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero * segundoNumero;
+
+console.log(operacaoMatematica);
+
+// ponto flutuante
+
+const numeroPontoFluante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFluante;
+console.log(novaOperacao);
+const novaOperacao2 = numeroPontoFluante / pontoFlutuanteSemZero;
+console.log(novaOperacao2);
+
+// Nan --> Not a number
+
+const alura = 'Alura';
+console.log(alura * primeiroNumero);
+
+const expoente = 10E2;
+console.log(expoente);
---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá mundo!";
+const texto2 = 'olá mundo!';
+const senha = "senhaSuperSegura123";
+const stringNumeros = "345676";
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Thiago';
+
+// concatenacao (+)
+
+console.log(citacao + meuNome);
+
+
+
+// template string ou template literal
+
