74bb51343459dddbb21eed6c31a9fed3f255e741
Thiago Andrade <thicma@gmail.com>
2022-05-03 14:51:25 -0300

    Atualizacao

---
+++ type-boolean.js
@@ -0,0 +1,10 @@
+const primeiroNumero = 5;
+const segundoNumero = 10;
+
+console.log(primeiroNumero < segundoNumero);
+
+const texto1 = 'Alura';
+const texto2 = 'alura';
+
+console.log(texto1.length === texto2.length);
+console.log(texto1.length + texto2.length);
